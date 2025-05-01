3fb4bf72971c78e0d85c4269a4aefe591550e642
yemokoo <104205235+yemokoo@users.noreply.github.com>
2025-05-01 22:12:34 +0900

    Delete inference/Benchmark/M3DocVQA directory

--- inference/Benchmark/M3DocVQA/m3docvqa_test.sh
+++
@@ -1,6 +0,0 @@
-accelerate launch --num_processes=1 --main_process_port=12346 -m lmms_eval \
-    --model qwen2_5_vl \
-    --model_args=pretrained=Qwen/Qwen2.5-VL-3B-Instruct,max_pixels=200704,use_flash_attention_2=True,device_map=auto\
-    --tasks m3docvqa_val \
-    --batch_size 1 \
-    --output_path /home/sem/lmms-eval/outputs/Qwen2_5_VL_m3docvqa_val.json
